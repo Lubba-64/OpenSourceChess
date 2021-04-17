5736166d7b8f1fdaebb317adc468ae5ee6eb4adc
Benjamin Hines <lubba64@gmail.com>
2021-04-17 14:44:00 -0400

    removed completely unnecessary backup scripts from repo

--- backup/DocumentControl.js
+++
@@ -1,6 +0,0 @@
-document.onmousemove = function(e){ 
-    var x = e.clientX; 
-    var y = e.clientY; 
-    document.getElementById('HeldPieceIMG').style.marginLeft  = x+"px";
-    document.getElementById('HeldPieceIMG').style.marginTop  = y+"px";
-}
